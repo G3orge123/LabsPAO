c371e9b9ab4b57b095c0e05fc0e32bc751282d69
Ilie George Cristian <127673934+G3orge123@users.noreply.github.com>
2024-05-16 11:10:03 +0300

    Delete PROIECT_PAO directory

--- PROIECT_PAO/src/AbonamentStandard.java
+++
@@ -1,7 +0,0 @@
-package cobraKaiDojo;
-
-public class AbonamentStandard extends Abonament {
-    public AbonamentStandard() {
-        super("Standard", 100.0, 1, "Acces la antrenamente de grup.");
-    }
-}
